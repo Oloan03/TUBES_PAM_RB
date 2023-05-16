7ddc3c38b177efae6b34fc4353b595ca8fa589a4
Kafindo Ebenov Simbolon <104490808+KafESimbolon@users.noreply.github.com>
2023-05-16 17:18:38 +0700

    Update App.js

--- News/App.js
+++ News/App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './HomeScreen';
 import DetailScreen from './DetailScreen';
+import SettingsScreen from './Settings';
 
 const Stack = createStackNavigator();
 
