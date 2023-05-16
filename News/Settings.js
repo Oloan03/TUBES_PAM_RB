afd4e77ed65b3e2262ff3cd9c9bd7412fd49754c
Kafindo Ebenov Simbolon <104490808+KafESimbolon@users.noreply.github.com>
2023-05-16 17:21:58 +0700

    Update Settings.js

--- News/Settings.js
+++ News/Settings.js
@@ -28,25 +28,31 @@ function SettingsScreen({ onBack }) {
   return (
     <View style={{ flex: 1, padding: 10 }}>
       <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center', height: 60 }}>
+        /*
         <TouchableOpacity style={{ position: 'absolute', left: 10 }} onPress={goBack}>
           <Image source={require('../assets/arrow.png')} style={{ width: 24, height: 24 }} />
         </TouchableOpacity>
+        */
 
         <Text style={{ fontSize: 18, fontWeight: 'bold' }}>SETTINGS</Text>
       </View>
 
       <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 10 }}>
+        /*
         <TouchableOpacity style={{ marginRight: 10 }}>
           <Image source={require('../assets/mode.png')} style={{ width: 24, height: 24 }} />
         </TouchableOpacity>
+        */
         <Text style={{ marginRight: 'auto' }}>Mode</Text>
         <Switch value={mode === 'dark'} onValueChange={toggleMode} />
       </View>
 
       <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 10 }}>
+        /*
         <TouchableOpacity style={{ marginRight: 10 }}>
           <Image source={require('../assets/notif.png')} style={{ width: 24, height: 24}} />
         </TouchableOpacity>
+        */
         <Text style={{ marginRight: 'auto' }}>Notifications</Text>
         <Switch value={false} onValueChange={toggleNotifications} />
       </View>
