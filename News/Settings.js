cb05b8746603c31fa638c142f9991227bba11b99
Kafindo Ebenov Simbolon <104490808+KafESimbolon@users.noreply.github.com>
2023-05-19 12:33:49 +0700

    Update Settings.js

--- News/Settings.js
+++ News/Settings.js
@@ -28,36 +28,37 @@ function SettingsScreen({ onBack }) {
   return (
     <View style={{ flex: 1, padding: 10 }}>
       <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center', height: 60 }}>
-        /*
+        
         <TouchableOpacity style={{ position: 'absolute', left: 10 }} onPress={goBack}>
-          <Image source={require('../assets/arrow.png')} style={{ width: 24, height: 24 }} />
+          <Image source={require('./assets/arrow.png')} style={{ width: 24, height: 24 }} />
         </TouchableOpacity>
-        */
+        
 
         <Text style={{ fontSize: 18, fontWeight: 'bold' }}>SETTINGS</Text>
       </View>
 
       <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 10 }}>
-        /*
+        
         <TouchableOpacity style={{ marginRight: 10 }}>
-          <Image source={require('../assets/mode.png')} style={{ width: 24, height: 24 }} />
+          <Image source={require('./assets/mode.png')} style={{ width: 24, height: 24 }} />
         </TouchableOpacity>
-        */
+        
         <Text style={{ marginRight: 'auto' }}>Mode</Text>
         <Switch value={mode === 'dark'} onValueChange={toggleMode} />
       </View>
 
       <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 10 }}>
-        /*
+        
         <TouchableOpacity style={{ marginRight: 10 }}>
-          <Image source={require('../assets/notif.png')} style={{ width: 24, height: 24}} />
+          <Image source={require('./assets/notif.png')} style={{ width: 24, height: 24}} />
         </TouchableOpacity>
-        */
+        
         <Text style={{ marginRight: 'auto' }}>Notifications</Text>
         <Switch value={false} onValueChange={toggleNotifications} />
       </View>
 
       <TouchableOpacity style={{ marginTop: 10 }} onPress={goToAbout}>
+        <Image source={require('./assets/about.png')} style={{ width: 24, height: 24 }} />
         <Text style={{ color: '#007AFF' }}>About app</Text>
       </TouchableOpacity>
     </View>
